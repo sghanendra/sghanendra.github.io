b83028db7f9bec2c9ebe3d4c266b5094cbd88b7a
sghanendra <57911691+sghanendra@users.noreply.github.com>
2026-04-06 15:38:25 +0000

    Deploying to gh-pages from @ sghanendra/sghanendra.github.io@06e781d1174be7d5a3456c61dceb80d747d31dc0 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -408,9 +408,9 @@ ninja.data = [{
           description: "Switching dynamics of Working Memory",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-cortical-field-model",
+          title: 'Cortical Field Model',
+          description: "Complex Spiral Traveling Waves",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
