46133d4ce3c3d413ec4ee15b44bee9c532e5b77c
sghanendra <57911691+sghanendra@users.noreply.github.com>
2026-04-06 15:29:31 +0000

    Deploying to gh-pages from @ sghanendra/sghanendra.github.io@9bd09074c0e455cffec3784920fabf3186c7a262 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-publications",
           title: "publications",
-          description: "publications papers and bioRiv.",
+          description: "Papers and bioRiv.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
