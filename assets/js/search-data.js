0368f1fc760e3bf8490d51502ad3d2143aa7aeb9
sghanendra <57911691+sghanendra@users.noreply.github.com>
2026-04-06 15:35:39 +0000

    Deploying to gh-pages from @ sghanendra/sghanendra.github.io@2b4dc1cb343e572346a083f847968e4c9e2c6f07 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -403,9 +403,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-working-memory",
+          title: 'Working Memory',
+          description: "Switching dynamics of Working Memory",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
